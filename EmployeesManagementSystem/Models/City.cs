9eb7bcb196caf2c358912e2622f3afe5c18a55f6
OnurAydin02 <07onuraydin@gmail.com>
2024-10-28 00:20:18 +0300

    İnitial Commit

---
+++ EmployeesManagementSystem/Models/City.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeesManagementSystem.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string CountryId { get; set; }
+        public Country Country { get; set; }
+
+    }
+}
